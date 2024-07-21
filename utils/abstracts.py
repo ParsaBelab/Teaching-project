30576289bf2766980bb0a4ccb02f4013b5216d1f
Parsa Belab <165410165+ParsaBelab@users.noreply.github.com>
2024-07-21 13:02:54 +0330

    add jalali time to project

--- utils/abstracts.py
+++ utils/abstracts.py
@@ -1,4 +1,5 @@
 from django.db import models
+from persiantools.jdatetime import JalaliDate
 
 
 class AbstractDateTime(models.Model):
@@ -7,3 +8,11 @@ class AbstractDateTime(models.Model):
 
     class Meta:
         abstract = True
+    @property
+    def get_created_persian(self):
+        jcreated = JalaliDate(self.created,locale='fa')
+        return jcreated.strftime('%c',locale='fa')
+    @property
+    def get_updated_persian(self):
+        jupdated = JalaliDate(self.updated,locale='fa')
+        return jupdated
